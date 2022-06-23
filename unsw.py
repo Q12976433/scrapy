6518a2d0a5dba08d2082ff8aae76863a032b38bc
mm <misxu94@gmail.com>
2022-06-23 20:34:42 +1000

    main_scrapy

---
+++ unsw.py
@@ -0,0 +1,78 @@
+#-*- coding:utf-8 -*-
+"""
+----------
+# create of datetime:2022/6/6 21:13
+----------
+# create of software: PyCharm
+"""
+import requests
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+from lxml import etree
+
+
+def get_html(url):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
+    }
+    res = requests.get(url,headers=headers).content.decode()
+    return etree.HTML(res)
+
+def xpath_html(html,pattern,list1):
+    divs = html.xpath(pattern)
+    for div in divs:
+        content = ''.join(div.xpath(".//text()")).replace("\n", '').strip().replace("   ", '').replace("Learn more.",'').replace("Year one",'\n1.').replace("Year two",'\n2.').replace("Year three",'\n3.').replace("Year four",'\n4.').replace("Year five",'\n5.').replace("Year six",'\n6.').replace("Year seven",'\n7.')
+        if content != '':
+            list1.append(content)
+
+def parse1(i):
+    list1 = {}
+    href1 = i['displayUrl']
+    html = get_html(href1)
+    list1['url'] = href1
+    list1['name'] = html.xpath("//h1/text()")[0]
+    list1['desc'] = ''.join(html.xpath("//h1/following-sibling::p/text()"))
+    name_divs = html.xpath(
+        "//div[contains(@class,'propertytiles property-tiles-bg-colour--light-grey section')]/div[contains(@class,'property-tiles')]/div")
+    for name_div in name_divs:
+        name = ''.join(name_div.xpath("./div[1]//text()")).replace("\n", '').strip()
+        value = ''.join(name_div.xpath("./div[2]//text()")).replace("\n", '').strip()
+        list1[name] = value
+    name_divs1 = html.xpath("//li[@tabindex='0']/span/@data-click_name")
+    j = 1
+    for name_div1 in name_divs1:
+        name_div1 = name_div1.replace("&", '').replace("  ", '_').replace(" ", '_').replace("?", '').lower()
+        if 'fees' in name_div1:
+            list1['domestic_first_year_full_fee'] = ''.join(html.xpath("//div[contains(text(),'Indicative First Year Full Fee')]/following-sibling::div/text()")).replace("\n",'').strip()
+            list1['domestic_full_fee_to_complete_degree'] = ''.join(html.xpath("//div[contains(text(),'Indicative Full Fee to Complete Degree')]/following-sibling::div/text()")).replace("\n",'').strip()
+            list1['international_first_year_fee'] = ''.join(html.xpath("//div[contains(text(),'Indicative First Year Fee')]/following-sibling::div/text()")).replace("\n",'').strip()
+            list1['international_fee_to_complete_degree'] = ''.join(html.xpath("//div[contains(text(),'Indicative Fee to Complete Degree')]/following-sibling::div/text()")).replace("\n",'').strip()
+        else:
+            content = html.xpath(f"//div[@role='tabpanel'][position()={j}]//text()")
+            content = [c.strip() for c in content if c.strip()!='']
+            content = '\t'.join(content).replace("\n",'').strip().replace("   ", '').replace("Learn more.", '').replace("Year one", '\n1.').replace("Year two", '\n2.').replace("Year three", '\n3.').replace("Year four", '\n4.').replace("Year five", '\n5.').replace("Year six",'\n6.').replace("Year seven", '\n7.')
+            list1[name_div1] = content.replace("Ready to start your application?Apply nowFor most international students, applications are submitted via\xa0ourApply Onlineservice. We encourage you to submit your completed application as early as possible to ensure it will be processed in time for your preferred term.Some high-demand programs with limited places, may have an earlier application deadline or may have an earlier commencement\xa0date. For\xa0more information visit\xa0ourinternational applicant information page.*If you are an international student studying an Australian qualification, go to theUniversities Admission Centre (UAC)for application and UAC key dates. Note: If you are under 18 years of age, you need to make special arrangements.Read more.Ready to start your application?Apply now",'')
+        j = j + 1
+    print(list1)
+    datas.append(list1)
+
+
+def parse(url):
+    lis = get_html(url).xpath("//div[@class='responsivegrid aem-GridColumn--default--none aem-GridColumn aem-GridColumn--default--4 aem-GridColumn--medium--12 aem-GridColumn--small--12 aem-GridColumn--offset--default--0']//ul/li")
+    for li in lis[:10]:
+        title = li.xpath("./a/@title")[0].replace(" ","+")
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
+        }
+        href = f'https://search.unsw.edu.au/s/search.html?form=json&collection=unsw~unsw-search&profile=degrees&query=!padrenull&start_rank=1&num_ranks=1000&f.Area+of+Study%7CdegreeAreaOfStudy={title}&gscope1=degree&cool.4=0.3'
+        r = requests.get(href,headers=headers).json()['response']['resultPacket']['results']
+        with ThreadPoolExecutor() as pool:
+            pool.map(parse1,r)
+    df = pd.DataFrame(datas)
+    df.to_excel("unsw.xlsx",index=False)
+
+
+if __name__ == '__main__':
+    datas = []
+    url = 'https://www.unsw.edu.au/study/find-a-degree-or-course'
+    parse(url)
---
+++ uts.py
@@ -0,0 +1,77 @@
+#-*- coding:utf-8 -*-
+"""
+----------
+# create of software: PyCharm
+"""
+import requests
+import threading
+import pandas as pd
+from lxml import etree
+
+
+def parse(tr,type):
+    i = 1
+    list = {}
+    for th in ths:
+        name = '_'.join(th.xpath(".//text()")[0].split(" "))
+        list['student_type'] = type[1]
+        list[f'{name}'] = ''.join(tr.xpath(f"./td[{i}]//text()")).strip()
+        i = i + 1
+    href = ''.join(tr.xpath("./td[1]/a/@href"))
+    if href != '':
+        try:
+            res = requests.get(href).text
+            html = etree.HTML(res)
+            list['location'] = ''.join(html.xpath("//em[contains(text(),'Location')]//text()")).replace("Location:",'').strip()
+            list['load_credit_points'] = ''.join(html.xpath("//em[contains(text(),'Load credit points')]//text()")).replace("Load credit points:", '')
+            list['url']=href
+            list['overview'] = ' '.join(html.xpath("//h2[2]/preceding-sibling::p[position()<last()]//text()")).strip()
+            print(list['location'])
+        except Exception as e:
+            with open("log.txt",'a',encoding='utf-8') as f:
+                f.write(f'{type[1]}{list["Course_Code"]}link:{href}has error\r:{e}\n')
+    else:
+        list['location'] = ''
+        list['load_credit_points'] = ''
+        list['overview'] = ''
+    list1.append(list)
+    print(list)
+    print("*" * 50)
+
+if __name__ == '__main__':
+    print("begin")
+    url = 'https://cis.uts.edu.au/fees/course-fees.cfm'
+    headers = {
+        'Host': 'cis.uts.edu.au',
+        'Origin': 'https://cis.uts.edu.au',
+        'Referer': 'https://cis.uts.edu.au/fees/course-fees.cfm',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
+    }
+    datas = [['HL', 'Commonwealth Supported Place'], ['DF', 'Postgraduate Domestic Coursework'],
+             ['DFRT', 'Postgraduate Domestic Research'], ['IFUG', 'lnternational Undergraduate'],
+             ['IF', 'International Postgraduate Coursework'], ['IFRT', 'International Postgraduate Research']]
+    list1 = []
+    for type in datas:
+        data = {
+            'fee_type': type[0],
+            'fee_year': 2022,
+            'cohort_year': 2022,
+            'course_area': 'All',
+            'Search':'',
+            'op': 'Search'
+        }
+        res = requests.post(url,headers=headers,data=data).text
+        html = etree.HTML(res)
+        ths = html.xpath("//tr[@valign='top'][1]/th")
+        trs = html.xpath("//tr[@valign='top'][position()>1]")
+        a = []
+        for tr in trs:
+            t = threading.Thread(target=parse,args=(tr,type))
+            t.start()
+            a.append(t)
+        [i.join() for i in a]
+        print(len(list1))
+    print("saving")
+    df = pd.DataFrame(list1)
+    df.to_excel("vsc3.xlsx",index=False)
+    print("over")
